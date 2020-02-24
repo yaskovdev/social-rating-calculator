2ed15a960682bf36bb4b7e9217d6ba7fc2af5eac
yaskovdev <yaskovdev@gmail.com>
2020-02-24 10:34:34 +0200

    Initial commit.

---
+++ src/test/java/com/yaskovdev/calculator/SocialRatingCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yaskovdev.calculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialRatingCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
